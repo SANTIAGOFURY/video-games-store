29733d7e4320be89733d2d532523b14d3a8b4fd2
Said Bouaziz <alphawork404@gmail.com>
2025-06-01 19:45:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+
+// sticky  nav  bar
+function stickyNavbar() {
+  header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+addEventListener("scroll", stickyNavbar);
